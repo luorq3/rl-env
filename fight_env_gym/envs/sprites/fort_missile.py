2a0da4d586647e73f2fae5bb80abf90ce0af4ada
ruiqingluo <ruiqingluo1@163.com>
2022-03-30 15:06:50 +0800

    # updates fort fire logic

--- fight_env_gym/envs/sprites/fort_missile.py
+++ fight_env_gym/envs/sprites/fort_missile.py
@@ -12,19 +12,20 @@ class FortMissile(Missile):
                  screen_size: Tuple[int, int],
                  rect: Rect,
                  radian: float,
-                 speed: int = 1):
+                 speed: int = 10):
         super(FortMissile, self).__init__(screen_size, rect, speed)
         self.radian = radian
         self.distance = 0
+        self.start_rect = self.rect.copy()
 
     def update(self):
         self.distance += self.speed
         offset_x = self.distance * math.sin(self.radian)
         offset_y = self.distance * math.cos(self.radian)
 
-        if 0 < self.rect.x - offset_x < self.max_rect[0] \
-                and 0 < self.rect.y - offset_y < self.max_rect[1]:
-            self.rect.x -= offset_x
-            self.rect.y -= offset_y
+        if 0 < self.start_rect.x - offset_x < self.max_rect[0] \
+                and 0 < self.start_rect.y - offset_y < self.max_rect[1]:
+            self.rect.x = self.start_rect.x - offset_x
+            self.rect.y = self.start_rect.y - offset_y
         else:
             self.kill()
