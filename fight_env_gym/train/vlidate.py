0bde78ff4cde31bf62966904d311b31d32c55c51
ruiqingluo <ruiqingluo1@163.com>
2022-04-05 16:56:39 +0800

    # validate logic

--- fight_env_gym/train/vlidate.py
+++ fight_env_gym/train/vlidate.py
@@ -7,7 +7,7 @@ from stable_baselines3.common.evaluation import evaluate_policy
 if __name__ == "__main__":
     env = fight_env_gym.make("Fight-rgb-v0")
 
-    model = A2C.load("dqn_lunar", env=env)
+    model = A2C.load("saved_model", env=env)
 
     mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
 
